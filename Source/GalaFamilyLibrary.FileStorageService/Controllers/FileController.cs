a3d72a0ebf3807d35400afb9a00572293f715eec
Young <iamlovedit@163.com>
2023-02-16 18:17:17 +0800

    add file storage project

---
+++ Source/GalaFamilyLibrary.FileStorageService/Controllers/FileController.cs
@@ -0,0 +1,35 @@
+﻿using GalaFamilyLibrary.Infrastructure.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace GalaFamilyLibrary.FileStorageService.Controllers;
+
+[Route("files")]
+public class FileController : ApiControllerBase
+{
+    private readonly IWebHostEnvironment _environment;
+
+    public FileController(IWebHostEnvironment webHostEnvironment)
+    {
+        _environment = webHostEnvironment;
+    }
+
+    [HttpGet("{*path:file}")]
+    public IActionResult Get(string path)
+    {
+        var filePath = Path.Combine(_environment.WebRootPath, "Files", path);
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound();
+        }
+
+        var provider = new FileExtensionContentTypeProvider();
+        if (!provider.TryGetContentType(filePath, out var contentType))
+        {
+            contentType = "application/octet-stream";
+        }
+
+
+        return PhysicalFile(filePath, contentType);
+    }
+}
