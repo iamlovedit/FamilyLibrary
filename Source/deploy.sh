90b0ce956aee7ec659dc65d13a8ee1790ccdee9a
Young <iamlovedit@163.com>
2023-02-20 17:04:25 +0800

    update

--- Source/deploy.sh
+++ Source/deploy.sh
@@ -1,9 +1,5 @@
-# shellcheck disable=SC2054
-array=(library_gateway,dynamo_package,family_library,library_identity)
-for ((i = 0; i < ${#array[@]}; i++)); do
-  docker stop ${array[i]}
-  docker rm ${array[i]}
-  docker rmi ${DOCKER_REGISTRY}/${array[i]}
-done
+COMPOSE_FILE=./docker-compose.prod.yml
 
-docker-compose -f docker-compose.prod.yml up -d
+docker-compose -f $COMPOSE_FILE pull
+docker-compose -f $COMPOSE_FILE down
+docker-compose -f $COMPOSE_FILE up -d
