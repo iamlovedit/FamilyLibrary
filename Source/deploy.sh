a7708940f74d30b0822b82a9cb9d6839e7658670
Young <iamlovedit@163.com>
2023-02-20 15:45:10 +0800

    update docker deploy config

--- Source/deploy.sh
+++ Source/deploy.sh
@@ -1,21 +1,9 @@
-gatewaycontainer=library_gateway
-packagecontainer=dynamo_package
-familycontainer=family_library
-identityContainer=library_identity
-
-docker stop $gatewaycontainer
-docker stop $packagecontainer
-docker stop $familycontainer
-docker stop $identityContainer
-
-docker rm $gatewaycontainer
-docker rm $packagecontainer
-docker rm $familycontainer
-docker rm $identityContainer
-
-docker rmi ${DOCKER_REGISTRY}library_gateway
-docker rmi ${DOCKER_REGISTRY}dynamo_package
-docker rmi ${DOCKER_REGISTRY}family_library
-docker rmi ${DOCKER_REGISTRY}library_identity
+# shellcheck disable=SC2054
+array=(library_gateway,dynamo_package,family_library,library_identity)
+for ((i = 0; i < ${#array[@]}; i++)); do
+  docker stop ${array[i]}
+  docker rm ${array[i]}
+  docker rmi ${DOCKER_REGISTRY}/${array[i]}
+done
 
 docker-compose -f docker-compose.prod.yml up -d
