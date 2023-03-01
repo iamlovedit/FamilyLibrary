b08e8aea70e31a0296f18106cb13e08a0fc91b83
Young <iamlovedit@163.com>
2023-03-01 10:50:02 +0800

    update

--- Source/deploy.sh
+++ Source/deploy.sh
@@ -1,5 +1,5 @@
 COMPOSE_FILE=./docker-compose.prod.yml
-
+export DOCKER_REGISTRY=registry.cn-chengdu.aliyuncs.com/galaservices/
 docker-compose -f $COMPOSE_FILE pull
 docker-compose -f $COMPOSE_FILE down --volumes --remove-orphans mysql redis seq seqcli
 docker-compose -f $COMPOSE_FILE up -d
