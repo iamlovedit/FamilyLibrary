2461aec7ba158dfb551fd1012ce1ba1050b60bee
Young <iamlovedit@163.com>
2023-03-02 15:21:03 +0800

    add TokenValidator

---
+++ Source/GalaFamilyLibrary.Infrastructure/Common/GalaTokenValidator.cs
@@ -0,0 +1,24 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GalaFamilyLibrary.Infrastructure.Common;
+
+public class GalaTokenValidator : ISecurityTokenValidator
+{
+    public bool CanReadToken(string securityToken)
+    {
+        return true;
+    }
+
+    public ClaimsPrincipal ValidateToken(string securityToken, TokenValidationParameters validationParameters,
+        out SecurityToken validatedToken)
+    {
+        ClaimsPrincipal principal =
+            new JwtSecurityTokenHandler().ValidateToken(securityToken, validationParameters, out validatedToken);
+        return principal;
+    }
+
+    public bool CanValidateToken { get; }
+    public int MaximumTokenSizeInBytes { get; set; }
+}
