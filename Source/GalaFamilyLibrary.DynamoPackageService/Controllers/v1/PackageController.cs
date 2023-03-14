a9e4762f34715fa649fbc24731d201b39c7dcc37
Young <iamlovedit@163.com>
2023-03-14 11:42:18 +0800

    fixed bugs

--- Source/GalaFamilyLibrary.DynamoPackageService/Controllers/v1/PackageController.cs
+++ Source/GalaFamilyLibrary.DynamoPackageService/Controllers/v1/PackageController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Linq.Expressions;
+using System.Net.Http;
 using AutoMapper;
 using GalaFamilyLibrary.DynamoPackageService.DataTransferObjects;
 using GalaFamilyLibrary.DynamoPackageService.Models;
 using GalaFamilyLibrary.DynamoPackageService.Services;
 using GalaFamilyLibrary.Infrastructure.Common;
 using GalaFamilyLibrary.Infrastructure.Redis;
+using GalaFamilyLibrary.Infrastructure.Seed;
+using GalaFamilyLibrary.Infrastructure.Transaction;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace GalaFamilyLibrary.DynamoPackageService.Controllers.v1;
 
@@ -85,5 +90,97 @@ public class PackageController : ApiControllerBase
         var result = packagesPage.ConvertTo<PackageDTO>(_mapper);
         await _redis.Set(redisKey, result, _redisRequirement.CacheTime);
         return SucceedPage(result);
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Administrator")]
+    public async Task<MessageModel<string>> UpdateAsync([FromServices] IHttpClientFactory clientFactory, [FromServices] AppDbContext appDbContext, [FromServices] IUnitOfWork unitOfWork)
+    {
+        var httpClient = clientFactory.CreateClient();
+        var responseMessage = await httpClient.GetAsync("https://dynamopackages.com/packages");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var json = await responseMessage?.Content?.ReadAsStringAsync();
+            if (!string.IsNullOrEmpty(json))
+            {
+                try
+                {
+                    var jObject = JObject.Parse(json);
+                    var content = jObject["content"];
+                    if (content is null)
+                    {
+                        return Failed<string>("获取原网站数据失败");
+                    }
+                    var newPackages = content.ToObject<List<DynamoPackage>>();
+                    var packageDb = appDbContext.GetEntityDB<DynamoPackage>();
+                    var packageVersionDb = appDbContext.GetEntityDB<PackageVersion>();
+                    var oldPackages = await packageDb.GetListAsync();
+                    var oldPackageVersions = await packageVersionDb.GetListAsync();
+                    unitOfWork.BeginTransaction();
+                    var addedPackages = new List<DynamoPackage>();
+                    var addedPackageVersions = new List<PackageVersion>();
+                    var newPackageVersions = new List<PackageVersion>();
+                    foreach (var package in newPackages)
+                    {
+                        var oldPackage = oldPackages.FirstOrDefault(p => p.Id == package.Id);
+                        if (oldPackage is null)
+                        {
+                            addedPackages.Add(package);
+                        }
+                        else
+                        {
+                            await packageDb.UpdateAsync(package);
+                        }
+
+                        foreach (var pVersion in package.Versions)
+                        {
+                            pVersion.PackageId = package.Id;
+                            var oldPackageVersion = oldPackageVersions.FirstOrDefault(pv => pv.PackageId == package.Id && pv.Version == pVersion.Version);
+                            if (oldPackageVersion is null)
+                            {
+                                addedPackageVersions.Add(pVersion);
+                            }
+                            else
+                            {
+                                await packageVersionDb.UpdateAsync(pVersion);
+                            }
+                            newPackageVersions.Add(pVersion);
+                        }
+                    }
+
+                    await packageDb.InsertRangeAsync(addedPackages);
+                    await packageVersionDb.InsertRangeAsync(addedPackageVersions);
+
+                    foreach (var package in oldPackages)
+                    {
+                        var newPackage = newPackages.FirstOrDefault(p => p.Id == package.Id);
+                        if (newPackage is null)
+                        {
+                            package.IsDeleted = true;
+                            await packageDb.UpdateAsync(package);
+                        }
+                    }
+                    foreach (var pVersion in oldPackageVersions)
+                    {
+                        var newVersion = newPackageVersions.FirstOrDefault(pv => pv.PackageId == pVersion.PackageId && pv.Version == pVersion.Version);
+                        if (newVersion is null)
+                        {
+                            pVersion.IsDeleted = true;
+                            await packageVersionDb.UpdateAsync(pVersion);
+                        }
+                    }
+                    _logger.LogInformation("added new package count {added},added new version count {addedverson}", addedPackages.Count, addedPackageVersions.Count);
+                    unitOfWork.CommitTransaction();
+                }
+                catch (Exception e)
+                {
+                    unitOfWork.RollbackTransaction();
+                    _logger.LogError(e, e.Message);
+                    return Failed(e.Message);
+                }
+                return Success("更新完成");
+            }
+        }
+        return Failed($"request failed,http status code {responseMessage.StatusCode}");
     }
 }
--- Source/GalaFamilyLibrary.DynamoPackageService/Jobs/FetchPackagesJob.cs
+++ Source/GalaFamilyLibrary.DynamoPackageService/Jobs/FetchPackagesJob.cs
@@ -25,10 +25,8 @@ namespace GalaFamilyLibrary.DynamoPackageService.Jobs
         {
             var httpClient = _httpClientFactory.CreateClient();
             var responseMessage = await httpClient.GetAsync("https://dynamopackages.com/packages");
-            _logger.LogInformation("start a task for update dynamo packages");
             if (responseMessage.IsSuccessStatusCode)
             {
-                //TODO:update database
                 var json = await responseMessage?.Content?.ReadAsStringAsync();
                 if (!string.IsNullOrEmpty(json))
                 {
@@ -36,67 +34,69 @@ namespace GalaFamilyLibrary.DynamoPackageService.Jobs
                     {
                         var jObject = JObject.Parse(json);
                         var content = jObject["content"];
-                        if (!string.IsNullOrEmpty(content?.ToString()))
+                        if (content is null)
                         {
-                            var newPackages = content.ToObject<List<DynamoPackage>>();
-                            var packageDb = _appDbContext.GetEntityDB<DynamoPackage>();
-                            var packageVersionDb = _appDbContext.GetEntityDB<PackageVersion>();
-                            var oldPackages = await packageDb.GetListAsync();
-                            var oldPackageVersions = await packageVersionDb.GetListAsync();
-                            _unitOfWork.BeginTransaction();
-                            var addedPackages = new List<DynamoPackage>();
-                            var addedPackageVersions = new List<PackageVersion>();
-                            var newPackageVersions = new List<PackageVersion>();
-                            foreach (var package in newPackages)
+                            return;
+                        }
+                        var newPackages = content.ToObject<List<DynamoPackage>>();
+                        var packageDb = _appDbContext.GetEntityDB<DynamoPackage>();
+                        var packageVersionDb = _appDbContext.GetEntityDB<PackageVersion>();
+                        var oldPackages = await packageDb.GetListAsync();
+                        var oldPackageVersions = await packageVersionDb.GetListAsync();
+                        _unitOfWork.BeginTransaction();
+                        var addedPackages = new List<DynamoPackage>();
+                        var addedPackageVersions = new List<PackageVersion>();
+                        var newPackageVersions = new List<PackageVersion>();
+                        foreach (var package in newPackages)
+                        {
+                            var oldPackage = oldPackages.FirstOrDefault(p => p.Id == package.Id);
+                            if (oldPackage is null)
                             {
-                                var oldPackage = oldPackages.FirstOrDefault(p => p.Id == package.Id);
-                                if (oldPackage is null)
+                                addedPackages.Add(package);
+                            }
+                            else
+                            {
+                                await packageDb.UpdateAsync(package);
+                            }
+
+                            foreach (var pVersion in package.Versions)
+                            {
+                                pVersion.PackageId = package.Id;
+                                var oldPackageVersion = oldPackageVersions.FirstOrDefault(pv => pv.PackageId == package.Id && pv.Version == pVersion.Version);
+                                if (oldPackageVersion is null)
                                 {
-                                    addedPackages.Add(package);
+                                    addedPackageVersions.Add(pVersion);
                                 }
                                 else
                                 {
-                                    await packageDb.UpdateAsync(package);
-                                }
-
-                                foreach (var pVersion in package.Versions)
-                                {
-                                    var oldPackageVersion = oldPackageVersions.FirstOrDefault(v => v.Id == pVersion.Id);
-                                    if (oldPackageVersion is null)
-                                    {
-                                        addedPackageVersions.Add(pVersion);
-                                    }
-                                    else
-                                    {
-                                        await packageVersionDb.UpdateAsync(pVersion);
-                                    }
-                                    newPackageVersions.Add(pVersion);
-                                }
-                            }
-                            _logger.LogInformation("added new package count {added},added new version count {addedverson}",addedPackages.Count,addedPackageVersions.Count);
-                            await packageDb.InsertRangeAsync(addedPackages);
-                            await packageVersionDb.InsertRangeAsync(addedPackageVersions);
-
-                            foreach (var package in oldPackages)
-                            {
-                                var newPackage = newPackages.FirstOrDefault(p => p.Id == package.Id);
-                                if (newPackage is null)
-                                {
-                                    package.IsDeleted = true;
-                                    await packageDb.UpdateAsync(package);
-                                }
-                            }
-                            foreach (var pVersion in oldPackageVersions)
-                            {
-                                var newVersion = newPackageVersions.FirstOrDefault(v => v.Id == pVersion.Id);
-                                if (newVersion is null)
-                                {
-                                    pVersion.IsDeleted = true;
                                     await packageVersionDb.UpdateAsync(pVersion);
                                 }
+                                newPackageVersions.Add(pVersion);
                             }
-                            _unitOfWork.CommitTransaction();
                         }
+                        await packageDb.InsertRangeAsync(addedPackages);
+                        await packageVersionDb.InsertRangeAsync(addedPackageVersions);
+
+                        foreach (var package in oldPackages)
+                        {
+                            var newPackage = newPackages.FirstOrDefault(p => p.Id == package.Id);
+                            if (newPackage is null)
+                            {
+                                package.IsDeleted = true;
+                                await packageDb.UpdateAsync(package);
+                            }
+                        }
+                        foreach (var pVersion in oldPackageVersions)
+                        {
+                            var newVersion = newPackageVersions.FirstOrDefault(pv => pv.PackageId == pVersion.PackageId && pv.Version == pVersion.Version);
+                            if (newVersion is null)
+                            {
+                                pVersion.IsDeleted = true;
+                                await packageVersionDb.UpdateAsync(pVersion);
+                            }
+                        }
+                        _logger.LogInformation("added new package count {added},added new version count {addedverson}", addedPackages.Count, addedPackageVersions.Count);
+                        _unitOfWork.CommitTransaction();
                     }
                     catch (Exception e)
                     {
@@ -105,6 +105,7 @@ namespace GalaFamilyLibrary.DynamoPackageService.Jobs
                     }
                 }
             }
+            _logger.LogError("request error,http status code : {code}", responseMessage.StatusCode);
         }
     }
 }
