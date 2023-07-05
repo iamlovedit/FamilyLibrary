3008259d020be9bcabe240c0ebce07960c6c1ee4
Young <iamlovedit@163.com>
2023-07-05 16:17:44 +0800

    add parameter mirco service

---
+++ Source/GalaFamilyLibrary.ParameterService/Services/IParameterService.cs
@@ -0,0 +1,28 @@
+﻿using GalaFamilyLibrary.Domain.Models.FamilyParameter;
+using GalaFamilyLibrary.Infrastructure.Repository;
+using GalaFamilyLibrary.Infrastructure.Service;
+
+namespace GalaFamilyLibrary.ParameterService.Services
+{
+    public interface IParameterService : IServiceBase<Parameter>
+    {
+        Task<Parameter> GetParameterDetailsAsync(long id);
+    }
+
+    class ParameterService : ServiceBase<Parameter>, IParameterService
+    {
+        public ParameterService(IRepositoryBase<Parameter> dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<Parameter> GetParameterDetailsAsync(long id)
+        {
+            return await DAL.DbContext.Queryable<Parameter>().
+                   Includes(f => f.Definition, d => d.ParameterGroup).
+                   Includes(f => f.Definition, d => d.ParameterType).
+                   Includes(f => f.Definition, d => d.UnitType).
+                   Includes(f => f.DisplayUnitType).
+                   InSingleAsync(id);
+        }
+    }
+}
---
+++ Source/GalaFamilyLibrary.ParameterService/Services/IParameterUnitTypeService.cs
@@ -0,0 +1,17 @@
+﻿using GalaFamilyLibrary.Domain.Models.FamilyParameter;
+using GalaFamilyLibrary.Infrastructure.Repository;
+using GalaFamilyLibrary.Infrastructure.Service;
+
+namespace GalaFamilyLibrary.ParameterService.Services
+{
+    public interface IParameterUnitTypeService : IServiceBase<ParameterUnitType>
+    {
+
+    }
+    public class ParameterUnitTypeService : ServiceBase<ParameterUnitType>, IParameterUnitTypeService
+    {
+        public ParameterUnitTypeService(IRepositoryBase<ParameterUnitType> dbContext) : base(dbContext)
+        {
+        }
+    }
+}
