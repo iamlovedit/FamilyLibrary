c19f3111b65e9f5a41d58259b8545f23fcc8bbc9
Young <iamlovedit@163.com>
2024-04-25 22:07:09 +0800

    removed unused files

--- Source/Frontend/src/views/HomeView.vue
+++ Source/Frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
