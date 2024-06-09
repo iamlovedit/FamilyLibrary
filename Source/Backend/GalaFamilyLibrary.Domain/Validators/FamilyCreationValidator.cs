9c2bd418e8e28faebd4695f5053029d762c625ce
Young <iamlovedit@163.com>
2024-06-09 23:41:31 +0800

    add validators

---
+++ Source/Backend/GalaFamilyLibrary.Domain/Validators/FamilyCreationValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using GalaFamilyLibrary.Domain.DataTransferObjects.FamilyLibrary;
+
+namespace GalaFamilyLibrary.Domain.Validators
+{
+    public class FamilyCreationValidator : AbstractValidator<FamilyCreationDTO>
+    {
+        public FamilyCreationValidator()
+        {
+            RuleFor(fc => fc.Name).NotNull().WithMessage("");
+            
+        }
+    }
+}
