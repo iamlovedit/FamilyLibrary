9a5e2b54f835a3705366fe52f34efc07ed1106eb
Young <iamlovedit@163.com>
2024-06-12 22:41:05 +0800

    update vite config

--- Source/Frontend/vite.config.ts
+++ Source/Frontend/vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import Unocss from 'unocss/vite'
 import { presetUno, presetAttributify, presetIcons } from 'unocss'
+import { env } from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +20,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: env.VITE_APP_API_BASE_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   }
 })
