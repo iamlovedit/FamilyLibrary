63bcaad9b1128f241ba0c3a40d5614fc66acd412
Young <iamlovedit@163.com>
2024-06-14 11:46:47 +0800

    optimise model

--- Source/Backend/GalaFamilyLibrary.Infrastructure/Common/MessageData.cs
+++ Source/Backend/GalaFamilyLibrary.Infrastructure/Common/MessageData.cs
@@ -1,19 +1,16 @@
-﻿using Newtonsoft.Json;
+﻿namespace GalaFamilyLibrary.Infrastructure.Common;
 
-namespace GalaFamilyLibrary.Infrastructure.Common;
-
-public class MessageData<T>(bool isSuccess, string message, int statusCode = 200)
+public class MessageData<T>(bool succeed, string message, int statusCode = 200)
 {
     public int StatusCode { get; set; } = statusCode;
 
-    public bool IsSuccess { get; } = isSuccess;
+    public bool Succeed { get; set; } = succeed;
 
-    public string Message { get; } = message;
+    public string? Message { get; } = message;
 
     public T? Response { get; }
 
-    public MessageData(bool isSuccess, string message, T? response, int statusCode = 200) : this(isSuccess, message,
-        statusCode)
+    public MessageData(bool succeed, string message, T response, int statusCode = 200) : this(succeed, message, statusCode)
     {
         Response = response;
     }
