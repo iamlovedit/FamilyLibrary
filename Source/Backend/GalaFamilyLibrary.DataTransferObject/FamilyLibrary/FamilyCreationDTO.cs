e2755d0f87b8c745fe7a8bc6b9a71af2288e9f5e
Young <iamlovedit@163.com>
2024-06-24 23:00:50 +0800

    update creation dto

--- Source/Backend/GalaFamilyLibrary.DataTransferObject/FamilyLibrary/FamilyCreationDTO.cs
+++ Source/Backend/GalaFamilyLibrary.DataTransferObject/FamilyLibrary/FamilyCreationDTO.cs
@@ -4,10 +4,10 @@
     {
         public string? Name { get; set; }
 
+        public string FileId { get; set; }
         public long UploaderId { get; set; }
 
         public string? Description { get; set; }
-
         public ushort Version { get; set; }
 
         public long CategoryId { get; set; }
