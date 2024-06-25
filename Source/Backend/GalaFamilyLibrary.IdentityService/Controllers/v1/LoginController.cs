a6a8de455553dcbcae181612757624e588d20bdd
Young <iamlovedit@163.com>
2024-06-26 00:02:37 +0800

    optimise codes

--- Source/Backend/GalaFamilyLibrary.IdentityService/Controllers/v1/LoginController.cs
+++ Source/Backend/GalaFamilyLibrary.IdentityService/Controllers/v1/LoginController.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Globalization;
+﻿using System.Globalization;
 using GalaFamilyLibrary.Infrastructure.Common;
 using GalaFamilyLibrary.Infrastructure.Redis;
 using GalaFamilyLibrary.Infrastructure.Security;
@@ -21,18 +20,13 @@ namespace GalaFamilyLibrary.IdentityService.Controllers.v1
     [ApiVersion("1.0")]
     [Route("identity/v{version:apiVersion}/auth")]
     public class LoginController(
-        PermissionRequirement permissionRequirement,
         ILogger<LoginController> logger,
         IRedisBasketRepository redis,
-        IAESEncryptionService aesEncryptionService,
         ITokenBuilder tokenBuilder,
         IUserService userService)
         : GalaControllerBase
     {
-        private readonly IRedisBasketRepository _redis = redis;
-        private readonly IAESEncryptionService _aesEncryptionService = aesEncryptionService;
-
-       [HttpPost("login")]
+        [HttpPost("login")]
         [AllowAnonymous]
         public async Task<MessageData<TokenInfo>> LoginAsync([FromBody] UserLoginDTO loginUser)
         {
@@ -40,18 +34,20 @@ namespace GalaFamilyLibrary.IdentityService.Controllers.v1
             {
                 return Failed<TokenInfo>("password or username is empty");
             }
+
             var lockKey = $"auth/username={loginUser.Username}&password={loginUser.Password}";
-            if (await _redis.Exist(lockKey))
+            if (await redis.Exist(lockKey))
             {
                 return Failed<TokenInfo>("invalid request", 400);
             }
 
-            await _redis.Set(lockKey, loginUser, TimeSpan.FromSeconds(1));
+            await redis.Set(lockKey, loginUser, TimeSpan.FromSeconds(1));
             var user = await userService.GetFirstByExpressionAsync(u => u.Username == loginUser.Username);
             if (user is null)
             {
                 return Failed<TokenInfo>("username or password is incorrect");
             }
+
             var password = loginUser.Password!.MD5Encrypt32(user.Salt!);
             if (password != user.Password)
             {
@@ -61,13 +57,15 @@ namespace GalaFamilyLibrary.IdentityService.Controllers.v1
             var roles = await userService.GetUserRolesAsync(user.Id);
 
             var claims = new List<Claim>()
-                {
-                    new(ClaimTypes.Name, user.Username!),
-                    new(JwtRegisteredClaimNames.Jti, user.Id.ObjToString()),
-                    new(JwtRegisteredClaimNames.Iat, EpochTime.GetIntDate(DateTime.Now).ToString(CultureInfo.InvariantCulture),ClaimValueTypes.Integer64),
-                    new(ClaimTypes.Expiration,
-                        DateTime.Now.AddSeconds(tokenBuilder.GetTokenExpirationSeconds()).ToString())
-                };
+            {
+                new(ClaimTypes.Name, user.Username!),
+                new(JwtRegisteredClaimNames.Jti, user.Id.ObjToString()),
+                new(JwtRegisteredClaimNames.Iat,
+                    EpochTime.GetIntDate(DateTime.Now).ToString(CultureInfo.InvariantCulture),
+                    ClaimValueTypes.Integer64),
+                new(ClaimTypes.Expiration,
+                    DateTime.Now.AddSeconds(tokenBuilder.GetTokenExpirationSeconds()).ToString())
+            };
             claims.AddRange(roles.Select(r => new Claim(ClaimTypes.Role, r.Name!)));
             var token = tokenBuilder.GenerateTokenInfo(claims);
             return Success(token);
@@ -77,16 +75,18 @@ namespace GalaFamilyLibrary.IdentityService.Controllers.v1
         public async Task<MessageData<TokenInfo>> RefreshTokenAsync([FromForm] string token)
         {
             var lockKey = $"auth/token/refresh/token={token}";
-            if (await _redis.Exist(lockKey))
+            if (await redis.Exist(lockKey))
             {
                 return Failed<TokenInfo>("invalid request");
             }
-            await _redis.Set(lockKey, token, TimeSpan.FromSeconds(1));
+
+            await redis.Set(lockKey, token, TimeSpan.FromSeconds(1));
 
             if (string.IsNullOrEmpty(token))
             {
                 return Failed<TokenInfo>("token is invalid");
             }
+
             token = tokenBuilder.DecryptCipherToken(token);
             var uid = tokenBuilder.ParseUIdFromToken(token);
             if (!tokenBuilder.VerifyToken(token) || uid <= 0)
@@ -99,12 +99,15 @@ namespace GalaFamilyLibrary.IdentityService.Controllers.v1
             {
                 return Failed<TokenInfo>("refresh failed");
             }
+
             var roles = await userService.GetUserRolesAsync(uid);
             var claims = new List<Claim>()
             {
                 new(ClaimTypes.Name, user.Username!),
                 new(JwtRegisteredClaimNames.Jti, user.Id.ObjToString()),
-                new(JwtRegisteredClaimNames.Iat, EpochTime.GetIntDate(DateTime.Now).ToString(CultureInfo.InvariantCulture),ClaimValueTypes.Integer64),
+                new(JwtRegisteredClaimNames.Iat,
+                    EpochTime.GetIntDate(DateTime.Now).ToString(CultureInfo.InvariantCulture),
+                    ClaimValueTypes.Integer64),
                 new(ClaimTypes.Expiration,
                     DateTime.Now.AddSeconds(tokenBuilder.GetTokenExpirationSeconds()).ToString())
             };
