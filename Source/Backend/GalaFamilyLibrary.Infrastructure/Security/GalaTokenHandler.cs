18e0307cc5f325201de2c8f9b0f5aa07fe0e44c4
Young <iamlovedit@163.com>
2024-06-26 22:46:01 +0800

    refactor class names

---
+++ Source/Backend/GalaFamilyLibrary.Infrastructure/Security/GalaTokenHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using GalaFamilyLibrary.Infrastructure.Security.Encyption;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GalaFamilyLibrary.Infrastructure.Security
+{
+    public class GalaTokenHandler(
+        IAESEncryptionService aesEncryptionService,
+        JwtSecurityTokenHandler jwtSecurityTokenHandler)
+        : TokenHandler
+    {
+        public override Task<TokenValidationResult> ValidateTokenAsync(string token, TokenValidationParameters validationParameters)
+        {
+            var decodeToken = aesEncryptionService.Decrypt(token);
+            return jwtSecurityTokenHandler.ValidateTokenAsync(decodeToken, validationParameters);
+        }
+    }
+}
