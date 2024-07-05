5f9d8be8a80d9ccf2efa9f239d2158e35b83ad14
Young <iamlovedit@163.com>
2024-07-05 11:06:39 +0800

    optimise router regex expression

--- Source/Frontend/src/router/modules/family.ts
+++ Source/Frontend/src/router/modules/family.ts
@@ -1,4 +1,4 @@
-import type { RouteRecordRaw } from 'vue-router'
+import type { RouteRecordRaw, Route } from 'vue-router'
 
 const familyRoutes: RouteRecordRaw[] = [
   {
@@ -22,10 +22,26 @@ const familyRoutes: RouteRecordRaw[] = [
       {
         path: 'broswer',
         name: 'family-browser',
-        component: () => import('@/views/family/broswer/index.vue')
+        component: () => import('@/views/family/broswer/index.vue'),
+        props: (route: Route) => ({
+          keyword: route.query.keyword || '',
+          categoryId: route.query.categoryId || '',
+          order: route.query.order || '',
+          page: parseInt(route.query.page) || 1,
+          pageSize: parseInt(route.query.pageSize) || 30
+        }),
+        beforeEnter: (to: Route, from: Route, next) => {
+          const page = parseInt(to.query.page)
+          const pageSize = parseInt(to.query.pageSize)
+          if (Number.isInteger(page) && Number.isInteger(pageSize)) {
+            next()
+          } else {
+            next('/404')
+          }
+        }
       },
       {
-        path: 'detail/:id(^[1-9][0-9]{18}$)',
+        path: 'detail/:id(\\d{19})',
         name: 'family-detail',
         component: () => import('@/views/family/detail/index.vue')
       }
