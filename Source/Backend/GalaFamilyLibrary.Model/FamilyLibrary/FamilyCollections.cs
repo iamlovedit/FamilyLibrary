9c8c49587096bd1e4db215dca7dc77075dd5a679
Young <iamlovedit@163.com>
2024-07-25 23:50:55 +0800

    added models

---
+++ Source/Backend/GalaFamilyLibrary.Model/FamilyLibrary/FamilyCollections.cs
@@ -0,0 +1,21 @@
+using GalaFamilyLibrary.Repository;
+using SqlSugar;
+
+namespace GalaFamilyLibrary.Model.FamilyLibrary
+{
+    [SugarTable(TableName = "library_family_collections")]
+    public class FamilyCollections:IDeletable
+    {
+        [SugarColumn(IsPrimaryKey = true, ColumnName = "family_collections_id")]
+        public long Id { get; set; }
+
+        [SugarColumn(ColumnName = "family_collections_userId")]
+        public long UserId { get; set; }
+
+        [SugarColumn(ColumnName = "family_collections_familyId")]
+        public long FamilyId { get; set; }
+
+        [SugarColumn(ColumnName = "family_collections_isDeleted")]
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Source/Backend/GalaFamilyLibrary.Model/FamilyLibrary/FamilyLikes.cs
@@ -0,0 +1,21 @@
+using GalaFamilyLibrary.Repository;
+using SqlSugar;
+
+namespace GalaFamilyLibrary.Model.FamilyLibrary
+{
+    [SugarTable(TableName = "library_family_likes")]
+    public class FamilyLikes : IDeletable
+    {
+        [SugarColumn(IsPrimaryKey = true, ColumnName = "family_likes_id")]
+        public long Id { get; set; }
+
+        [SugarColumn(ColumnName = "family_likes_userId")]
+        public long UserId { get; set; }
+
+        [SugarColumn(ColumnName = "family_likes_familyId")]
+        public long FamilyId { get; set; }
+
+        [SugarColumn(ColumnName = "family_likes_isDeleted")]
+        public bool IsDeleted { get; set; }
+    }
+}
