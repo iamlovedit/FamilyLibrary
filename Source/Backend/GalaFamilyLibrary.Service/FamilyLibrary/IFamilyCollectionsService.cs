a53d39ed729b0b773dde184defd152b647887f59
Young <iamlovedit@163.com>
2024-07-28 22:40:43 +0800

    added new methods

--- Source/Backend/GalaFamilyLibrary.Service/FamilyLibrary/IFamilyCollectionsService.cs
+++ Source/Backend/GalaFamilyLibrary.Service/FamilyLibrary/IFamilyCollectionsService.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using GalaFamilyLibrary.Model.Identity;
 using GalaFamilyLibrary.Repository;
 
@@ -5,10 +6,18 @@ namespace GalaFamilyLibrary.Service.FamilyLibrary
 {
     public interface IFamilyCollectionsService : IServiceBase<FamilyCollections>
     {
+        Task<FamilyCollections> GetCollectionByExpressionAsync(Expression<Func<FamilyCollections, bool>> expression);
     }
 
     public class FamilyCollectionsService(IRepositoryBase<FamilyCollections> dbContext)
         : ServiceBase<FamilyCollections>(dbContext), IFamilyCollectionsService
     {
+        public async Task<FamilyCollections> GetCollectionByExpressionAsync(
+            Expression<Func<FamilyCollections, bool>> expression)
+        {
+            return await DAL.DbContext.Queryable<FamilyCollections>()
+                .ClearFilter<IDeletable>()
+                .FirstAsync(expression);
+        }
     }
 }
--- Source/Backend/GalaFamilyLibrary.Service/FamilyLibrary/IFamilyLikesService.cs
+++ Source/Backend/GalaFamilyLibrary.Service/FamilyLibrary/IFamilyLikesService.cs
@@ -1,17 +1,22 @@
+using System.Linq.Expressions;
 using GalaFamilyLibrary.Model.FamilyLibrary;
 using GalaFamilyLibrary.Repository;
 
 namespace GalaFamilyLibrary.Service.FamilyLibrary
 {
-    public interface IFamilyLikesService:IServiceBase<FamilyLikes>
+    public interface IFamilyLikesService : IServiceBase<FamilyLikes>
     {
-        
+        Task<FamilyLikes> GetFamilyLikesByExpressionAsync(Expression<Func<FamilyLikes, bool>> expression);
     }
 
     public class FamilyLikesService(IRepositoryBase<FamilyLikes> dbContext)
         : ServiceBase<FamilyLikes>(dbContext), IFamilyLikesService
     {
-        
+        public async Task<FamilyLikes> GetFamilyLikesByExpressionAsync(Expression<Func<FamilyLikes, bool>> expression)
+        {
+            return await DAL.DbContext.Queryable<FamilyLikes>()
+                .ClearFilter<IDeletable>()
+                .FirstAsync(expression);
+        }
     }
-    
 }
