63535d28beb1d5c7a3598752594b0b079ac69700
Young <iamlovedit@163.com>
2024-08-11 16:22:28 +0800

    update home views

--- Source/Frontend/react/src/views/layout/index.tsx
+++ Source/Frontend/react/src/views/layout/index.tsx
@@ -1,28 +1,40 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate, useLocation } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
 import type { MenuProps } from 'antd';
+import { useState } from 'react';
+
 type MenuItem = Required<MenuProps>['items'][number];
 
 const items: MenuItem[] = [
     {
         label: '首页',
-        key: 'home'
+        key: '/'
     },
     {
         label: '节点包',
-        key: 'dynamo'
+        key: '/dynamo'
     },
     {
         label: '族库',
-        key: 'family'
+        key: '/family'
     }
 ]
 const { Header, Content, Footer } = Layout;
 function MainLayout() {
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+    const [currentItem, setCurrentItem] = useState(pathname)
+    const onClick: MenuProps['onClick'] = (e) => {
+        setCurrentItem(e.key);
+        navigate(e.key)
+    }
     return (
         <Layout className='h-full w-full flex flex-col flex-nowrap'>
-            <Header >
-                <Menu items={items} mode='horizontal' theme="dark" />
+            <Header>
+                <Menu items={items}
+                    onClick={onClick}
+                    selectedKeys={[currentItem]}
+                    mode='horizontal' theme="dark" />
             </Header>
             <Content>
                 <Outlet />
