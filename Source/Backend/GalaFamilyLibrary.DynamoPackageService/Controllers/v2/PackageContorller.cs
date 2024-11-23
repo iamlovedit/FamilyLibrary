d4c31a0f341ed0d2824c0693883b3f6693d30fe8
Young <iamlovedit@163.com>
2024-11-23 21:53:12 +0800

    updated services

--- Source/Backend/GalaFamilyLibrary.DynamoPackageService/Controllers/v2/PackageContorller.cs
+++ Source/Backend/GalaFamilyLibrary.DynamoPackageService/Controllers/v2/PackageContorller.cs
@@ -1,6 +1,6 @@
 ﻿using Asp.Versioning;
+using GalaFamilyLibrary.DataTransferObject.Package;
 using GalaFamilyLibrary.Infrastructure;
-using GalaFamilyLibrary.Infrastructure.Common;
 using GalaFamilyLibrary.Service.Package;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +11,13 @@ namespace GalaFamilyLibrary.DynamoPackageService.Controllers.v2;
 public class PackageController(IPublishedPackageService packageService, ILogger<PackageController> logger)
     : DefaultControllerBase
 {
-    
+    [HttpGet("all")]
+    public async Task<MessageData<PageData<PublishedPackageDto>>> GetPackagePageAsync(
+        string? keyword = null, int pageIndex = 1,
+        int pageSize = 30, string? orderBy = null)
+    {
+        var packagePage =
+            await packageService.GetPackagePageAsync(keyword, pageIndex, pageSize, orderBy);
+        return SucceedPage(packagePage);
+    }
 }
--- Source/Backend/GalaFamilyLibrary.Service/Package/IPublishedPackageService.cs
+++ Source/Backend/GalaFamilyLibrary.Service/Package/IPublishedPackageService.cs
@@ -1,15 +1,33 @@
+using GalaFamilyLibrary.DataTransferObject.Package;
+using GalaFamilyLibrary.Infrastructure;
+using GalaFamilyLibrary.Infrastructure.Redis;
 using GalaFamilyLibrary.Infrastructure.Repository.Mongo;
 using GalaFamilyLibrary.Model.Package;
+using Mapster;
+using SqlSugar;
 
 namespace GalaFamilyLibrary.Service.Package
 {
     public interface IPublishedPackageService : IMongoServiceBase<PublishedPackage, string>
     {
+        Task<PageData<PublishedPackageDto>> GetPackagePageAsync(string? keyword = null, int pageIndex = 1,
+            int pageSize = 30, string? orderBy = null);
     }
 
-    public class PublishedPackageService(IMongoRepositoryBase<PublishedPackage, string> repositoryBase)
+    public class PublishedPackageService(
+        IMongoRepositoryBase<PublishedPackage, string> repositoryBase,
+        IRedisBasketRepository redis)
         : MongoServiceBase<PublishedPackage, string>(repositoryBase), IPublishedPackageService
     {
-        
+        public async Task<PageData<PublishedPackageDto>> GetPackagePageAsync(string? keyword = null, int pageIndex = 1,
+            int pageSize = 30, string? orderBy = null)
+        {
+            var expression = Expressionable.Create<PublishedPackage>()
+                .AndIF(!string.IsNullOrEmpty(keyword),
+                    x => x.Name.Contains(keyword, StringComparison.CurrentCultureIgnoreCase))
+                .ToExpression();
+            var packagePage = await DAL.GetPageDataAsync(pageIndex, pageSize, expression, orderBy);
+            return packagePage.ConvertTo<PublishedPackageDto>();
+        }
     }
 }
