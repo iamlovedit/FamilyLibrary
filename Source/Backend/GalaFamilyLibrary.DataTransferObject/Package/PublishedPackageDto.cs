987bad8b8c28e47dd0fee233d215275b17a9f9e6
Young <iamlovedit@163.com>
2024-11-23 21:52:50 +0800

    added new dto

---
+++ Source/Backend/GalaFamilyLibrary.DataTransferObject/Package/PublishedPackageDto.cs
@@ -0,0 +1,25 @@
+namespace GalaFamilyLibrary.DataTransferObject.Package
+{
+    public class PublishedPackageDto
+    {
+        public string Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public DateTime Created { get; set; }
+        
+        public DateTime Updated { get; set; }
+        
+        public int Downloads { get; set; }
+
+        public int Votes { get; set; }
+        
+        public string Description { get; set; }
+
+        public string Engine { get; set; }
+
+        public string Group { get; set; }
+        
+        public int VersionCount { get; set; }
+    }
+}
