7bc3ca3059dbb48965cb6141566dcd3d38ebe934
Young <iamlovedit@163.com>
2024-11-25 00:22:28 +0800

    updated views

--- Source/Frontend/react/src/components/Header/index.tsx
+++ Source/Frontend/react/src/components/Header/index.tsx
@@ -10,13 +10,15 @@ import {
   DropdownTrigger,
   Dropdown,
   DropdownMenu,
-  Avatar
+  Avatar,
+  Button
 } from '@nextui-org/react'
 import { useState } from 'react'
 import { AcmeLogo } from '../Logo'
-import { SearchIcon } from '../SearchIcon'
-import { useLocation } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
+import { useTheme } from 'next-themes'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { MoonOutline, SunnyOutline } from '@ricons/ionicons5'
+import { Icon } from '@ricons/utils'
 
 type MenuItem = {
   name: string
@@ -25,6 +27,7 @@ type MenuItem = {
 
 function Header() {
   const location = useLocation()
+  const { theme, setTheme } = useTheme()
   const [activeItem, setActiveItem] = useState(location.pathname)
   const navigate = useNavigate()
 
@@ -70,49 +73,7 @@ function Header() {
           </NavbarItem>
         ))}
       </NavbarContent>
-      <NavbarContent as="div" className="items-center" justify="end">
-        <Input
-          classNames={{
-            base: 'max-w-full sm:max-w-[10rem] h-10',
-            mainWrapper: 'h-full',
-            input: 'text-small',
-            inputWrapper:
-              'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20'
-          }}
-          placeholder="Type to search..."
-          size="sm"
-          startContent={<SearchIcon size={18} />}
-          type="search"
-        />
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="secondary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
-            </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger">
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarContent>
+      <NavbarContent as="div" className="items-center" justify="end"></NavbarContent>
     </Navbar>
   )
 }
--- Source/Frontend/react/src/views/home/index.tsx
+++ Source/Frontend/react/src/views/home/index.tsx
@@ -1,23 +1,16 @@
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-import { Button } from '@nextui-org/react'
+import { useEffect, useState } from 'react'
 
 function Home() {
-
   const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
   useEffect(() => {
     setMounted(true)
   }, [])
 
-  if (!mounted) { return null }
+  if (!mounted) {
+    return null
+  }
 
-  return (
-    <>
-      <Button onClick={() => setTheme('light')}>Light Mode</Button>
-      <Button onClick={() => setTheme('dark')}>Dark Mode</Button>
-    </>
-  )
+  return <>{'Home view'}</>
 }
 
 export default Home
