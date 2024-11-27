56e72337d8501811a19c0cde620bce2c45bde529
Young <iamlovedit@163.com>
2024-11-27 22:56:55 +0800

    updated footer

--- Source/Frontend/react/src/components/Footer/index.tsx
+++ Source/Frontend/react/src/components/Footer/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
 function Footer() {
-    return (
-        <div>Footer</div>
-    )
+  return (
+    <div className="flex w-full h-14 border-t items-center justify-center border-divider bg-background/70">
+      <a href="https://beian.miit.gov.cn" target="_blank">
+        备案号：蜀ICP备2022029729号-1
+      </a>
+    </div>
+  )
 }
 
 export default Footer
