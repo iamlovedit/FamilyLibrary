cb723c53ebb2ba19a67f7c63129290c580bf2e70
Young <iamlovedit@163.com>
2024-12-20 22:42:00 +0800

    added extented properties

--- Source/Frontend/react/src/vite-env.d.ts
+++ Source/Frontend/react/src/vite-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_API_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
