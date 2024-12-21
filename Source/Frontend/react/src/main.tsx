6603ad6c8a686a8b3837f6b7e7b43bb3a4142b53
Young <iamlovedit@163.com>
2024-12-21 23:20:01 +0800

    added Toaster

--- Source/Frontend/react/src/main.tsx
+++ Source/Frontend/react/src/main.tsx
@@ -1,19 +1,21 @@
-import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'sonner'
 
 import App from './App.tsx'
 import './index.css'
-const rootContainer = ReactDOM.createRoot(document.getElementById('root')!);
+const rootContainer = ReactDOM.createRoot(document.getElementById('root')!)
 rootContainer.render(
   <React.StrictMode>
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
         <div className="text-foreground bg-background h-dvh w-full">
+          <Toaster position="top-center" />
           <App />
         </div>
       </NextThemesProvider>
     </NextUIProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
